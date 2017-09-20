074753b04aa5b7328140f00711d1b0e128e6081c
Tommy <kbztommy@gmail.com>
2017-09-20 14:19:31 +0800

    增加註解

--- TextMapper/src/iisi/tcd/textMapper/util/TextBeanUtil.java
+++ TextMapper/src/iisi/tcd/textMapper/util/TextBeanUtil.java
@@ -17,6 +17,15 @@ import iisi.tcd.textMapper.exception.TextMapperException;
 
 public class TextBeanUtil {
 
+	/**
+	 * 將文本轉換成物件。
+	 * 
+	 * @param text
+	 *            要轉換成物件的文本
+	 * @param clazz
+	 *            欲轉換的class
+	 * @return 符合格式則回傳物件，否則會拋出TextMapperException;
+	 */
 	public static <T> T parseBean(String text, Class<T> clazz) {
 
 		try {
@@ -63,6 +72,13 @@ public class TextBeanUtil {
 		}
 	}
 
+	/**
+	 * 將物件轉換成文本
+	 * 
+	 * @param bean
+	 *            欲轉換成文本的物件
+	 * @return 符合格式則回傳文本，否則會拋出TextMapperException;
+	 */
 	public static String parseString(Object bean) {
 		Class clazz = bean.getClass();
 		if (null == clazz.getDeclaredAnnotation(TextBean.class)) {
